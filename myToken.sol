59626d6cae9713e99b82e9a5e3f8ca9fa3a9b871
inileshsharma <108680890+inileshsharma@users.noreply.github.com>
2023-07-21 23:59:22 +0530

    Add files via upload

---
+++ myToken.sol
@@ -0,0 +1,58 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+contract MyFirstToken {
+
+    string public TkName;
+    string public TkSymbol;
+    uint256 public TotalSupply;
+
+
+    // mapping function 
+    mapping(address => uint)public balanceOf;
+   
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Burn(address indexed from, uint value);
+    event Mint(address indexed to, uint value);
+
+    constructor(string memory _EnterTkname, string memory _EnterTksymbol) {
+        TkName = _EnterTkname;
+        TkSymbol = _EnterTksymbol;
+        TotalSupply = 0;
+    }
+
+
+    function mint(address to, uint value) public  {
+    
+        require(value > 0, "Please Enter a valid Amount");
+
+        TotalSupply += value;
+        balanceOf[to] += value;
+
+        emit Mint(to, value);
+        
+    }
+
+    function burn(uint256 value) public  {
+
+        require(balanceOf[msg.sender] >= value, "YOU CAN ONLY BURN AMOUNT LESS OR EQUAL TO CURRENT BALANCE");
+
+        TotalSupply -= value;
+        balanceOf[msg.sender] -= value;
+
+        emit Burn(msg.sender, value);
+       
+    }
+
+    function transfer(address to, uint value) external returns (bool) {
+        
+        require(balanceOf[msg.sender] >= value, "YOUR ARE TRANSFERING MORE THAN CURRENT BALANCE");
+
+        balanceOf[msg.sender] -= value;
+        balanceOf[to] += value;
+
+        emit Transfer(msg.sender, to, value);
+        return true;
+    }
+}
